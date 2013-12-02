2210f11664ea9cb982836ca46f1bb2665e5fbb08
莫兮 <moxi@zhihu.com>
2013-12-02 19:23:17 +0800

    update template path

--- wishlist/moxisite/settings.py
+++ wishlist/moxisite/settings.py
@@ -1,4 +1,7 @@
 # Django settings for moxisite project.
+import os
+
+PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -110,7 +113,7 @@ ROOT_URLCONF = 'moxisite.urls'
 WSGI_APPLICATION = 'moxisite.wsgi.application'
 
 TEMPLATE_DIRS = (
-    '/Users/zhongnakakei/Documents/Django/wishlist/templates/',
+    os.path.join(PROJECT_PATH, 'templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
