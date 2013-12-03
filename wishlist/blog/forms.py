e897303548645255b13f680dfa331960c8b7caa7
莫兮 <moxi@zhihu.com>
2013-12-03 11:07:22 +0800

    widgets

--- wishlist/blog/forms.py
+++ wishlist/blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms.extras.widgets import SelectDateWidget
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from blog.models import Blog
 
 
@@ -8,6 +8,9 @@ class BlogForm(ModelForm):
     class Meta:
         model = Blog
         exclude = ('author',)
+        widgets = {
+            'body_text': Textarea(attrs={'cols': 10, 'rows': 10}),
+        }
 
 class UserForm(forms.Form):
     username = forms.CharField(max_length=255)
